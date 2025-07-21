9452d682b820d88d3a28d375faebd2de1443cade
Zarak Khan <119649492+Zarakkhan-dev@users.noreply.github.com>
2025-07-21 12:42:30 +0500

    route added

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password/:token" element={<ResetPassword />} /> */}
         <Route 
-          path="/manage" 
+          path="/manages" 
           element={
             <ProtectedRoute>
               <ManageIncidents />
